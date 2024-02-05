3b80188440caac73a09da9966d140f3b3653a259
Grazieli Francisco <129116654+GraziFrancisco@users.noreply.github.com>
2024-02-05 19:46:58 -0300

    conf lista de receitas

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Bolo Nega Maluca', 'Delicioso bolo de chocolate, fácil de fazer', 'https://i.pinimg.com/736x/0b/1a/a7/0b1aa7407617f271085e721fa1f06a9b.jpg'),
+    new Recipe('Torta de Franco', 'Deliciosa torta de frango com requeijão, fácil de fazer', 'https://coolicias.com.br/wp-content/uploads/2023/06/Torta-de-frango-de-liquidificador-1024x683-optimized.jpg')
+    
+  ];
+  
+
+  
+}
