f60113dfcc4e0574028a1245232971ec0deb792d
Grazieli Francisco <129116654+GraziFrancisco@users.noreply.github.com>
2024-02-06 11:12:20 -0300

    fim sessão 4

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Ovos', 3, 'unidades'),
+    new Ingredient('Farinha', 1, 'xícara'),
+    new Ingredient('Leite', 1, 'xícara'),
+    new Ingredient('Margarina', 2, 'colheres'),
+    new Ingredient('Chocolate em pó', 4, 'colheres'),
+    new Ingredient('Fermento em pó', 1, 'colher'),
+    
+  ];
 
+    
 }
